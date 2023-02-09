3f6e10f5a9cfb37157580364a0990ce00b44c0bb
Manuel Šarić <manujelko@gmail.com>
2023-02-09 20:04:05 +0100

    Starting with the switch project logic

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,37 @@
 class Tooltip {}
 
-class ProjectItem {}
+class ProjectItem {
+  constructor(id) {
+    this.id = id;
+    this.connectMoreInfoButton();
+    this.connectSwitchButton();
+  }
+
+  connectMoreInfoButton() {}
+
+  connectSwitchButton() {
+    const projItemElement = document.getElementById(this.id);
+    const switchBtn = projItemElement.querySelector("button:last-of-type");
+    switchBtn.addEventListener("click");
+  }
+}
 
 class ProjectList {
+  projects = [];
+
   constructor(type) {
     const prjItems = document.querySelectorAll(`#${type}-projects li`);
-    console.log(prjItems);
+    for (const prjItem of prjItems) {
+      this.projects.push(new ProjectItem(prjItem.id));
+    }
+    console.log(this.projects);
+  }
+
+  addProject() {}
+
+  switchProject(projectId) {
+    this.addProject();
+    this.projects = this.projects.filter((p) => p.id !== projectsId);
   }
 }
 
